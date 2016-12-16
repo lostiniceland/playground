1424113ce4705ce0ba5c0bfce9d45d519b724a90
Marc Schlegel <marc.schlegel@gmx.de>
2016-12-16 13:23:03 +0100

    Update PingPong.scala

--- akka-demo/src/main/scala/playground/akka/PingPong.scala
+++ akka-demo/src/main/scala/playground/akka/PingPong.scala
@@ -9,15 +9,7 @@ case object PongMessage
 case object StartMessage
 case object StopMessage
 
-/**
-  * An Akka Actor example written by Alvin Alexander of
-  * http://devdaily.com
-  *
-  * Shared here under the terms of the Creative Commons
-  * Attribution Share-Alike License: http://creativecommons.org/licenses/by-sa/2.5/
-  *
-  * more akka info: http://doc.akka.io/docs/akka/snapshot/scala/actors.html
-  */
+
 class Ping(pong: ActorRef) extends Actor {
   var count = 0
   def incrementAndPrint() { count += 1; println("ping") }
