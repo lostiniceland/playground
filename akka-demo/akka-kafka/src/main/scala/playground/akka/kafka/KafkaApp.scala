a289f6f16abec7cb2b3ca2e3ac8a60c7ec85de8f
Marc Schlegel <marc.schlegel@gmx.de>
2017-01-07 13:03:47 +0100

    DeadLetter subscriber added

--- akka-demo/akka-kafka/src/main/scala/playground/akka/kafka/KafkaApp.scala
+++ akka-demo/akka-kafka/src/main/scala/playground/akka/kafka/KafkaApp.scala
@@ -1,13 +1,16 @@
 package playground.akka.kafka
 
-import akka.actor.{ActorRef, ActorSystem, Props}
+import akka.actor.{ActorRef, ActorSystem, DeadLetter, Props}
 import playground.akka.Reaper.WatchMe
-import playground.akka.{ProductionReaper, StartMessage, StopMessage}
+import playground.akka.{DeadLetterSubscriber, ProductionReaper, StartMessage, StopMessage}
 
 import scala.io.StdIn
 
 object KafkaApp extends App {
   val system = ActorSystem("KafkaSystem")
+  // Register  DeadLetter-Listener via EventStream
+  val sysListener = system.actorOf(Props[DeadLetterSubscriber], "sysListener")
+  system.eventStream.subscribe(sysListener, classOf[DeadLetter])
   // Build our reaper
   val reaper = system.actorOf(Props(new ProductionReaper()))
   val kafka: ActorRef = system.actorOf(Props[Kafka])
