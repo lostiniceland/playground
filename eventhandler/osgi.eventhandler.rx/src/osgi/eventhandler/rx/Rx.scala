3c812dc9124cb6875e46000f47a0a36fa03a1691
Marc Schlegel <marc.schlegel@gmx.de>
2017-03-20 23:03:37 +0100

    Prepared Vertx and RxJava

---
+++ eventhandler/osgi.eventhandler.rx/src/osgi/eventhandler/rx/Rx.scala
@@ -0,0 +1,12 @@
+package osgi.eventhandler.rx
+
+import io.reactivex.Observable
+import org.osgi.service.component.annotations.{Activate, Component}
+
+class Rx {
+
+  def activate(): Unit = {
+    Observable.fromArray(_)
+  }
+
+}
---
+++ eventhandler/osgi.eventhandler.vertx/src/osgi/eventhandler/vertx/TestVerticle.scala
@@ -0,0 +1,8 @@
+package osgi.eventhandler.vertx
+
+import io.vertx.core.AbstractVerticle
+
+class TestVerticle extends AbstractVerticle {
+
+  override def start(): Unit = super.start()
+}
